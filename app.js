e7009cd29391a9aafd9f88a7393cecacdf13d79e
anushkadeshpande <anushkaa5000@gmail.com>
2020-05-02 16:04:51 +0530

    update

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 window.addEventListener('load',()=>{
      let long;
      let lat;
-     
+     let temperatureDescription = document.querySelector(".temperature-description");
+     let temperatureDegree = document.querySelector(".temperature-degree");     
+     let locationTimezone = document.querySelector(".location-timezone");
      if(navigator.geolocation){
           navigator.geolocation.getCurrentPosition(position=>{
                long = position.coords.longitude;
                lat = position.coords.latitude;
 
-               //console.log(lat,long);
-               const proxy = 'https://cors-anywhere.herokuapp.com/';
-               const api = '${proxy}api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f';
+               const proxy = "https://cors-anywhere.herokuapp.com/";
+               const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f`;
           
                fetch(api)
                .then(response =>{
@@ -17,6 +18,11 @@ window.addEventListener('load',()=>{
                })
                .then(data =>{
                     console.log(data);
+                    const {temp}= data.main;
+
+                    //set DOM elements from api
+                    temperatureDegree.textContent = temp;
+
                });
           });
      }
