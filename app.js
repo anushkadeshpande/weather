28e608721a60457feafc62361b189852f832444f
anushkadeshpande <anushkaa5000@gmail.com>
2020-05-02 16:07:23 +0530

    update 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ window.addEventListener('load',()=>{
                lat = position.coords.latitude;
 
                const proxy = "https://cors-anywhere.herokuapp.com/";
-               const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f`;
+               const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f`;
           
                fetch(api)
                .then(response =>{
