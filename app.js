f7c97b7501cd3820069b87eab0b95c57b62f948b
anushkadeshpande <anushkaa5000@gmail.com>
2020-05-03 18:29:35 +0530

    update:loading effect

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-window.addEventListener('load',()=>{
-     let long;
-     let lat;
+window.addEventListener('load',myfun());
+let long=0;
+let lat=0;
+navigator.geolocation.getCurrentPosition((position=>{
+
+     long = position.coords.longitude;
+     lat = position.coords.latitude;}));
+
+//const funcall = document.querySelector('.myDiv');
+//funcall.addEventListener = ('showPage' , 'weather');
+
+var myVar;
+
+function myfun() {
+  myVar = setTimeout(showPage, 10000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  if(lat!=0 && long!=0)
+  {
+     document.getElementById("myDiv").style.display = "block";
+     weather();
+  }
+  else
+     {
+          let errormsg = document.querySelector(".error");
+          let msg = "We're unable to get your location :(";
+          errormsg.textContent = msg;
+     }
+}
+function weather(){
      let temperatureDescription = document.querySelector(".temperature-description");
      let temperatureDegree = document.querySelector(".temperature-degree");     
      let locationTimezone = document.querySelector(".location-timezone");
      let exact = document.querySelector(".exact-location");
 
      if(navigator.geolocation){
-          navigator.geolocation.getCurrentPosition(position=>{
-               long = position.coords.longitude;
-               lat = position.coords.latitude;
-
+          
                const proxy = "https://cors-anywhere.herokuapp.com/";
                const locate= `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f`
                const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}?units=si`;
@@ -22,8 +48,6 @@ window.addEventListener('load',()=>{
                .then(data =>{
                     console.log(data);
                     const {temperature , summary ,icon}= data.currently;
-                    const loc = data.name;
-                    const des = data.description;
 
                     //set DOM elements from api
                     temperatureDegree.textContent = temperature;
@@ -46,12 +70,7 @@ window.addEventListener('load',()=>{
                     exact.textContent = loc;
                     
                });
-          });
-
-     }
-     else{
-          locationTimezone.textContent = "We're unable to get your location :("
-     }
+          }
 
      function setIcons(icon,iconID){
           const skycons = new Skycons({color : "white"}); 
@@ -59,4 +78,4 @@ window.addEventListener('load',()=>{
           skycons.play();
           return skycons.set(iconID, Skycons[currentIcon]);
      }
-});
+}
