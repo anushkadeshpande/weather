cc82bee3b10c2ef6fbdc17293f7c43e9a23408b1
anushkadeshpande <anushkaa5000@gmail.com>
2021-01-03 20:33:10 +0530

    added loader

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 window.addEventListener('load', myfun());
-let long = 0;
-let lat = 0;
-navigator.geolocation.getCurrentPosition((position => {
-
-     long = position.coords.longitude;
-     lat = position.coords.latitude;
-}));
 
 //const funcall = document.querySelector('.myDiv');
 //funcall.addEventListener = ('showPage' , 'weather');
@@ -13,46 +6,58 @@ navigator.geolocation.getCurrentPosition((position => {
 var myVar;
 
 function myfun() {
-     myVar = setTimeout(showPage, 10000);
+
+     document.getElementById("myDiv").style.display = "none";
+     myVar = setTimeout(showPage, 1);
 }
 
 function showPage() {
-     document.getElementById("loader").style.display = "none";
-     if (lat != 0 && long != 0) {
-          document.getElementById("myDiv").style.display = "block";
-          weather();
-     }
-     else {
-          let errormsg = document.querySelector(".error");
-          let msg = "We're unable to get your location :(";
-          errormsg.textContent = msg;
-     }
+     let long = 0;
+     let lat = 0;
+
+     navigator.geolocation.getCurrentPosition((position => {
+
+          long = position.coords.longitude;
+          lat = position.coords.latitude;
+          if (lat != 0 && long != 0) {
+               //   document.getElementById("myDiv").style.display = "block";
+               weather(lat, long);
+          }
+          else {
+               let errormsg = document.querySelector(".error");
+               let msg = "We're unable to get your location :(";
+               errormsg.textContent = msg;
+          }
+     }));
+
+
 }
-function weather() {
+function weather(lat, long) {
      let temperatureDescription = document.querySelector(".temperature-description");
      let temperatureDegree = document.querySelector(".temperature-degree");
      let locationTimezone = document.querySelector(".location-timezone");
      let exact = document.querySelector(".exact-location");
-     let i=0;
-     let hourly=document.querySelector(".hourly");
+     let i = 0;
+     let hourly = document.querySelector(".hourly");
      if (navigator.geolocation) {
 
           const proxy = "https://cors-anywhere.herokuapp.com/";
           const locate = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cf3aa3fa484d8f88b76c34b064eb484f`
           const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}?units=si`;
-          
+
           fetch(locate)
-          .then(response => {
-               return response.json();
-          })
-          .then(data => {
-               console.log(data);
-               const loc = data.name;
-               
-               //set DOM elements from api
-               exact.textContent = loc;
-              
-          });
+               .then(response => {
+                    return response.json();
+               })
+               .then(data => {
+                    console.log(data);
+                    const loc = data.name;
+
+                    //set DOM elements from api
+                    exact.textContent = loc;
+                    document.getElementById("loader").style.display = "none";
+                    document.getElementById("myDiv").style.display = "block";
+               });
           fetch(api)
                .then(response => {
                     return response.json();
@@ -69,7 +74,7 @@ function weather() {
 
                });
 
-          
+
      }
 
      function setIcons(icon, iconID) {
@@ -79,20 +84,20 @@ function weather() {
           return skycons.set(iconID, Skycons[currentIcon]);
      }
 }
-function time(){
+function time() {
      var today = new Date();
      var h = today.getHours()
-     if (h>12) {h= h- "12"} ;
+     if (h > 12) { h = h - "12" };
      var m = today.getMinutes();
      var s = today.getSeconds();
      m = checkTime(m);
      s = checkTime(s);
      document.getElementById('clocky').innerHTML =
-     h + ":" + m + ":" + s;
-     
+          h + ":" + m + ":" + s;
+
      var t = setTimeout(time, 500);
-     }
-     function checkTime(i) {
-     if (i < 10) {i = "0" + i};
+}
+function checkTime(i) {
+     if (i < 10) { i = "0" + i };
      return i;
-     }
+}
